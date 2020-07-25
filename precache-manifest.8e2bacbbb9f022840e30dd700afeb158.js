2452c9cc97efaf99ca1ea0e072f6ab43f3445970
SJagwan <shubhanshu1997jagwan@gmail.com>
2020-07-25 23:23:29 +0530

    Updates

---
+++ precache-manifest.8e2bacbbb9f022840e30dd700afeb158.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61ef6c44c74c7dd79e75c43c408c244e",
+    "url": "/SJ_Portfolio/index.html"
+  },
+  {
+    "revision": "41f59e9039348a3bd6d7",
+    "url": "/SJ_Portfolio/static/css/main.970f8506.chunk.css"
+  },
+  {
+    "revision": "c2cbbf2dae89f2a6c714",
+    "url": "/SJ_Portfolio/static/js/2.1e5a2b92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SJ_Portfolio/static/js/2.1e5a2b92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41f59e9039348a3bd6d7",
+    "url": "/SJ_Portfolio/static/js/main.d927845f.chunk.js"
+  },
+  {
+    "revision": "95c41cb2b470acab1b70",
+    "url": "/SJ_Portfolio/static/js/runtime-main.9e6a3f5f.js"
+  },
+  {
+    "revision": "61fa1d71693eedaa9b21dda53a1ea64a",
+    "url": "/SJ_Portfolio/static/media/about.61fa1d71.png"
+  },
+  {
+    "revision": "5e359995654eacf3b1e2541edd48fb86",
+    "url": "/SJ_Portfolio/static/media/aboutme.5e359995.png"
+  },
+  {
+    "revision": "6ab507360870361c9ed822076cd0b695",
+    "url": "/SJ_Portfolio/static/media/icon.6ab50736.svg"
+  }
+]);
